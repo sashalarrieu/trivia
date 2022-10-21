b912745746f6404f6523c9316cbca6b42035a3af
sashalarrieu <sasha.larrieu@epsi.fr>
2022-10-21 12:30:25 +0200

    test sasha

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -1,15 +1,32 @@
 require('./game.js');
 
-describe("The test environment", function() {
-  it("should pass", function() {
-    expect(true).toBe(true);
-  });
+// describe("The test environment", function() {
+//   it("should pass", function() {
+//     expect(true).toBe(true);
+//   });
 
-  it("should access game", function() {
-    expect(Game).toBeDefined();
-  });
-});
+//   it("should access game", function() {
+//     expect(Game).toBeDefined();
+//   });
+// });
 
-describe("Your specs...", function() {
-  // it ...
-});
+// describe("Your specs...", function() {
+//   // it ...
+// });
+
+// describe("Players number -------------------------------------------------------------------------------", function() {
+//   it("should not pass because not enough players", function() {
+//     let game = new Game();
+//     game.add('charly');
+//     expect(game.isPlayable(game.howManyPlayers())).toBe(false);
+//   });
+
+//   it("should not pass because not enough players", function() {
+//     let game = new Game();
+//     game.add('Charly');
+//     game.add('Sasha');
+//     expect(game.isPlayable(game.howManyPlayers())).toBe(true);
+//   });
+
+// });
+
